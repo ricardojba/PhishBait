e11651cd2e901ee41645c4aa05acc9020d43555f
Ricardo Almeida <ricardojba@users.noreply.github.com>
2015-08-07 10:07:01 +0100

    Update linkedin_harvest.py

--- linkedin_harvest.py
+++ linkedin_harvest.py
@@ -2,14 +2,17 @@
 
 import re
 import sys
+#import urllib
 
 if len(sys.argv) != 4:
     print "\n*************************************************"
     print "**  LinkedIn Email Harvester                   **"
     print "**  by H@ck1tHu7ch (Justin Hutchens)           **"
+    print "**  little changes by vibrio (Ricardo Almeida) **"
     print "**  ...GONE PHISHING!!!                        **"
     print "*************************************************\n\n"
-    print "\n\nUsage - ./linkedin_harvest.py [Filename] [Format num] [suffix]"
+    print "First browse LinkedIn through Burp, save all responses to a file and after that run this\n"
+    print "Usage - ./linkedin_harvest.py [Filename] [Format num] [suffix]"
     print "\nFORMATS:"
     print "1 - [first].[last]@[suffix]"
     print "2 - [first][last]@[suffix]"
@@ -22,46 +25,84 @@ in_file = str(sys.argv[1])
 format = int(sys.argv[2])
 suffix = str(sys.argv[3]).lower()
 
-
 def get_names(in_file):
-    ## Getting 1st Degree Contacts (first name & last name)
-    f = open(in_file,'r')
-    strings = re.findall(r'"(\w*\s\w*)(\sis\syour\sconnection)',f.read())
 
-    ## Getting 2nd Degree Contacts
-    f = open(in_file,'r')
-    strings = strings + re.findall(r'"(\w*\s\w*)(\sis\sa\s2nd\sdegree\scontact)',f.read())
+    ##########################################################################################################
+    #with open(in_file) as f:
+    #    st = urllib.unquote_plus(f.read())
+    #    print st
+    ##########################################################################################################
 
-    ## Create List of Names
+    # Getting All Contacts from the linkedin advanced search (first name, middle & last name)
+    # NOTE: Doesn't handle fucky string encoding very well :P
+    f = open(in_file,'r')
+    strings = re.findall(r'(profileName\=)(\w*\+\w*)',f.read(), re.UNICODE)
+    f = open(in_file,'r')
+    strings = strings + re.findall(r'(profileName\=)(\w*\+\w*\+\w*)',f.read(), re.UNICODE)
+    # Create List of Names
     names = []
-    for x in strings:
-        names.append(str(x[0]))
+    for x in set(strings):
+        names.append((str(x[1]).replace("+", " ")))
     return names
+
+    ##########################################################################################################
+    # Old stuff
+    # Getting 1st Degree Contacts (first name, middle & last name)
+    #f = open(in_file,'r')
+    #strings = re.findall(r'"(\w*\s\w*)(\sis\syour\sconnection)',f.read())
+    #f = open(in_file,'r')
+    #strings = re.findall(r'"(\w*\s\w*\s\w*)(\sis\syour\sconnection)',f.read())
+
+    # Getting 2nd Degree Contacts (first name, middle & last name)
+    #f = open(in_file,'r')
+    #strings = strings + re.findall(r'"(\w*\s\w*)(\sis\sa\s2nd\sdegree\scontact)',f.read())
+    #f = open(in_file,'r')
+    #strings = strings + re.findall(r'"(\w*\s\w*\s\w*)(\sis\sa\s2nd\sdegree\scontact)',f.read())
+
+    # Getting 3rd Degree Contacts (first name, middle & last name)
+    #f = open(in_file,'r')
+    #strings = strings + re.findall(r'"(\w*\s\w*)(\sis\sa\s3rd\sdegree\scontact)',f.read())
+    #f = open(in_file,'r')
+    #strings = strings + re.findall(r'"(\w*\s\w*\s\w*)(\sis\sa\s3rd\sdegree\scontact)',f.read())
+
+    # Create List of Names
+    #names = []
+    #for x in strings:
+    #    names.append(str(x[0]))
+    #return names
 
 def format_1(names,suffix):
     emails = []
     for x in names:
         first = x.split(' ')[0].lower()
-        last = x.split(' ')[1].lower()
+        last = x.split(' ')[-1].lower()
         emails.append(first + '.' + last + '@' + suffix)
+    print '----------------------------------'
+    print 'Number of names extracted: ' + str(len(names))
+    print '----------------------------------'
     return emails
 
 def format_2(names,suffix):
     emails = []
     for x in names:
         first = x.split(' ')[0].lower()
-        last = x.split(' ')[1].lower()
+        last = x.split(' ')[-1].lower()
         emails.append(first + last + '@' + suffix)
+    print '----------------------------------'
+    print 'Number of names extracted: ' + str(len(names))
+    print '----------------------------------'
     return emails
 
 def format_3(names,suffix):
     emails = []
     for x in names:
         first = x.split(' ')[0].lower()
-        last = x.split(' ')[1].lower()
+        last = x.split(' ')[-1].lower()
         emails.append(first[0] + last + '@' + suffix)
+    print '----------------------------------'
+    print 'Number of names extracted: ' + str(len(names))
+    print '----------------------------------'
     return emails
-
 
 names = get_names(in_file)
 
